99e9088138393b3646a905a2422d69ca53e6f4a9
Snowowl2015 <danielgilnoga@gmail.com>
2019-07-15 00:17:41 -0300

    Alterei o label: "Vamo dale"

--- HelloWorld/HelloWorld/Form1.Designer.cs
+++ HelloWorld/HelloWorld/Form1.Designer.cs
@@ -35,18 +35,21 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(132, 108);
+            this.label1.Location = new System.Drawing.Point(329, 194);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(501, 55);
+            this.label1.Size = new System.Drawing.Size(379, 82);
             this.label1.TabIndex = 0;
-            this.label1.Text = "douglas da o bumbum";
+            this.label1.Text = "Vamo dale";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- HelloWorld/HelloWorld/Form1.cs
+++ HelloWorld/HelloWorld/Form1.cs
@@ -16,5 +16,10 @@ namespace HelloWorld
         {
             InitializeComponent();
         }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
